83d3cd730745230063181cbbe0dbe4ce96f02a71
Koffi-Sk <kofdzakou1411@gmail.com>
2022-05-28 11:28:02 -0400

    Code CSS et affichage des informations de mon api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const result = document.getElementById("result");
+const form = document.querySelector("form");
 let meals = [];
 
+// Appel de mon api
 async function fetchMeals() {
   await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=duck").then(
     (response) => response.json().then((data) => (meals = data.meals))
@@ -8,4 +11,25 @@ async function fetchMeals() {
   console.log(meals);
 }
 
-fetchMeals();
+//fetchMeals();
+
+function mealsDisplay() {
+  meals.length = 12;
+
+  result.innerHTML = meals.map(
+    (meal) =>
+      `
+        <li class="card">
+            <h2>${meal.strMeal}</h2>
+            <p>${meal.strArea}</p>
+            <img src=${meal.strMealThumb} alt="photo ${meal.strMeal}" />
+            <ul></ul>
+        </li>
+        `
+  );
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetchMeals().then(() => mealsDisplay());
+});
